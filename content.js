d6f81011635162807d082f420979f8ad18c7e529
pranavcpawar <pranavcpawar@outlook.com>
2024-11-18 01:29:24 +0530

    autofill data on load

---
+++ content.js
@@ -0,0 +1,76 @@
+function fillLumaForm() {
+	const form = document.querySelector("form");
+	const inputs = form.querySelectorAll(".luma-input:not(.has-indicator)");
+	const labels = form.querySelectorAll(".lux-input-label");
+
+	chrome.storage.local.get(null, function (data) {
+		let fields = [];
+
+		for (let key in data) {
+			if (key.startsWith("ts_")) {
+				fields.push({
+					name: data[key].name,
+					value: data[key].value,
+				});
+			}
+		}
+
+		console.log("saved data:", fields);
+
+		for (let i = 0; i < inputs.length; i++) {
+			const input = inputs[i];
+			const label = input.id
+				? form.querySelector(`label[for="${input.id}"]`)
+				: labels[i];
+
+			console.log("label:", label);
+			const labelText = label.textContent.includes(" *")
+				? label.textContent.split(" *")[0].replace("?", "")
+				: label.textContent.replace("?", "");
+			console.log("labelText:", labelText);
+
+			for (const field of fields) {
+				console.log("field:", field);
+				const regex = Array.isArray(field.name)
+					? new RegExp(`(?=.*\\b${field.name.join("\\b)(?=.*\\b")}\\b)`, "i")
+					: new RegExp(`(?=.*\\b${field.name}\\b)`, "i");
+
+				const isValid = regex.test(labelText);
+
+				if (isValid) {
+					console.log("match found:", field);
+					input.value = field.value;
+					input.dispatchEvent(new Event("input", { bubbles: true }));
+					break;
+				}
+			}
+		}
+	});
+}
+
+function detectLumaForm() {
+	const buttons = document.querySelectorAll("button");
+
+	buttons.forEach((button) => {
+		if (button.textContent.includes("Request to Join")) {
+			button.addEventListener("click", function () {
+				const observer = new MutationObserver(function (mutations) {
+					const mutation = mutations[0];
+					if (mutation.type === "childList") {
+						const form = document.querySelector("form");
+						if (form) {
+							console.log("Form detected");
+							fillLumaForm();
+							observer.disconnect();
+						}
+					}
+				});
+				observer.observe(button, { childList: true, subtree: true });
+			});
+		}
+	});
+}
+
+window.addEventListener("load", function () {
+	detectLumaForm();
+});
