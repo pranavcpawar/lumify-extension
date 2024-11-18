65959de41dccefb128bb5db87e6406728709ddd7
pranavcpawar <pranavcpawar@outlook.com>
2024-11-18 19:26:08 +0530

    add delete entry logic

--- src/interface/index.js
+++ src/interface/index.js
@@ -13,11 +13,34 @@ function renderData(fields) {
 				" && "
 			)}</span><span class="field__value">${
 				field.value
-			}</span></div><div class="field__actions"><img class="field__actions__button" src="/src/assets/edit.svg" alt="edit" /><img class="field__actions__button" src="/src/assets/delete.svg" alt="delete" /></div></div>`;
+			}</span></div><div class="field__actions"><img id="edit-${
+				field.ts
+			}" class="field__actions__button" src="/src/assets/edit.svg" alt="edit" /><img id="delete-${
+				field.ts
+			}" class="field__actions__button" src="/src/assets/delete.svg" alt="delete" /></div></div>`;
 		} else {
-			fieldElement.innerHTML = `<div class="field__container"><div class="field__content"><span class="field__name">${field.name}</span><span class="field__value">${field.value}</span></div><div class="field__actions"><img class="field__actions__button" src="/src/assets/edit.svg" alt="edit" /><img class="field__actions__button" src="/src/assets/delete.svg" alt="delete" /></div></div>`;
+			fieldElement.innerHTML = `<div class="field__container"><div class="field__content"><span class="field__name">${field.name}</span><span class="field__value">${field.value}</span></div><div class="field__actions"><img id="edit-${field.ts}" class="field__actions__button" src="/src/assets/edit.svg" alt="edit" /><img id="delete-${field.ts}" class="field__actions__button" src="/src/assets/delete.svg" alt="delete" /></div></div>`;
 		}
 		dashboard.appendChild(fieldElement);
+	}
+}
+
+function actions() {
+	const buttons = document.querySelectorAll(".field__actions__button");
+	console.log("buttons:", buttons);
+
+	for (const button of buttons) {
+		button.addEventListener("click", function () {
+			console.log("button:", button);
+			if (button.id.startsWith("delete-")) {
+				const ts = button.id.split("-")[1];
+				console.log("ts:", ts);
+				chrome.storage.local.remove(ts, function () {
+					console.log("removed");
+					loadData();
+				});
+			}
+		});
 	}
 }
 
@@ -29,6 +52,7 @@ function loadData() {
 			console.log("key:", key);
 			if (key.startsWith("ts_")) {
 				fields.push({
+					ts: key,
 					name: data[key].name,
 					value: data[key].value,
 				});
@@ -37,6 +61,7 @@ function loadData() {
 		console.log("saved data:", fields);
 
 		renderData(fields.reverse());
+		actions();
 	});
 }
 
